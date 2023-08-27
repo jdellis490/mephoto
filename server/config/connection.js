b4b126781cb08fd38c186e3f480e8f86f0d10414
Justin Ellis <jdellis490@gmail.com>
2023-08-27 09:12:40 -0700

    connecting db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const dbURI = process.env.MONGODB_URI;
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/neupixl-db",
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
