af2839f2c22e9ef09f53f03f5ff6229638757cf5
Justin Ellis <jdellis490@gmail.com>
2023-08-27 10:53:29 -0700

    server edit

--- server/server.js
+++ server/server.js
@@ -23,14 +23,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(express.static("public"));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"))
-});
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// app.use(express.static("public"));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "public", "index.html"))
 // });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
