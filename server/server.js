77fcc57e0cce39caeebf3d2dcfabdbe0e14d6aa9
Justin Ellis <jdellis490@gmail.com>
2023-08-27 10:47:49 -0700

    build edit

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"))
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
