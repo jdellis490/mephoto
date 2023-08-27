3eb6e1f14c9a8e89207bba0112fc35f3ca5117c1
Justin Ellis <jdellis490@gmail.com>
2023-08-27 10:23:13 -0700

    production changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbURI = process.env.MONGODB_URI;
 
 mongoose.connect(
-  process.env.MONGODB_URI,
+  dbURI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
