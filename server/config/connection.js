de4bcd677c688f657dde19c388b664a3a516ef38
Justin Ellis <jdellis490@gmail.com>
2023-09-06 18:46:31 -0700

    please work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dbURI = process.env.MONGODB_URI ;
+const dbURI = (process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/neupixl-db');
 
 mongoose.connect(
   dbURI,
